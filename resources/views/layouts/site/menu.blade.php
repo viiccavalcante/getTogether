85c0f6a6b26bd482e36402dddaac885ebc2e50d6
victoria <victoriapcavalcante@gmail.com>
2024-11-28 16:51:25 +0100

    adjusting menu

--- resources/views/layouts/site/menu.blade.php
+++ resources/views/layouts/site/menu.blade.php
@@ -8,55 +8,20 @@
                 </div>
                 <div class="hidden md:block">
                     <div class="ml-10 flex items-baseline space-x-4">
-
                         @foreach($menu as $title => $link)
                             <a href="{{$link}}" class="rounded-md  px-3 py-2 text-sm font-medium text-purple-200" aria-current="page">{{$title}}</a>
                         @endforeach
                     </div>
                 </div>
             </div>
-            <!-- <div class="hidden md:block">
+            <div class="hidden md:block">
                 <div class="ml-4 flex items-center md:ml-6">
-
-                    <!-- Profile dropdown -->
-                    <!-- <div class="relative ml-3">
-                        <div>
-                            <button
-                                @click="profile_menu = !profile_menu"
-                                type="button"
-                                class="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                                <span class="absolute -inset-1.5"></span>
-                                <span class="sr-only">Open user menu</span>
-                                <img class="h-8 w-8 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                            </button>
-                        </div>
-
-                        <div x-show="profile_menu" @click.away="profile_menu=false"
-                             class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                            <!-- Active: "bg-gray-100 outline-none", Not Active: "" -->
-                          <!--  <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-                            <a href="#" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-                            <form class="block px-4 py-2 text-sm text-gray-700" action="{{route('logout')}}" method="post">@csrf <button>Sign out</button></form>
-                        </div>
-
+                    <div class="relative ml-3">
+                        <form class="block px-4 py-2 text-sm text-white" action="{{route('logout')}}" method="post">@csrf <button>Sign out</button></form>
                     </div>
                 </div>
-            </div> -->
-            <div class="-mr-2 flex md:hidden">
-                <!-- Mobile menu button -->
-                <button @click="mobile_menu_open = !mobile_menu_open" type="button" class="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" aria-controls="mobile-menu" aria-expanded="false">
-                    <span class="absolute -inset-0.5"></span>
-                    <span class="sr-only">Open main menu</span>
-                    <!-- Menu open: "hidden", Menu closed: "block" -->
-                    <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                    </svg>
-                    <!-- Menu open: "block", Menu closed: "hidden" -->
-                    <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-                    </svg>
-                </button>
             </div>
+           
         </div>
     </div>
 
@@ -69,19 +34,8 @@
 
         </div>
         <div class="border-t border-gray-700 pb-3 pt-4">
-            <div class="flex items-center px-5">
-                <div class="shrink-0">
-                    <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
-                </div>
-                <div class="ml-3">
-                    <div class="text-base/5 font-medium text-white">Tom Cook</div>
-                    <div class="text-sm font-medium text-gray-400">tom@example.com</div>
-                </div>
-            </div>
             <div class="mt-3 space-y-1 px-2">
-                <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Your Profile</a>
-                <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Settings</a>
-                <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">Sign out</a>
+            <form class="block px-4 py-2 text-sm text-white" action="{{route('logout')}}" method="post">@csrf <button>Sign out</button></form>
             </div>
         </div>
     </div>
