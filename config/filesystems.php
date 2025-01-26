f35dcb544c1dacbcf9592d1e08eb17ae9240fa9f
victoria <victoriapcavalcante@gmail.com>
2025-01-26 14:48:55 +0100

    backup

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,7 +45,7 @@ return [
             'throw' => false,
         ],
 
-        's3' => [
+        'sharpay-evans' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
